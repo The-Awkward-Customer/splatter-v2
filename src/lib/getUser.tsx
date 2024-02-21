ae60f38f3e9e937ba0024021f17305b6e913aee3
PETER ABBOTT <abbotsayshi@gmail.com>
2024-02-21 15:18:51 +0000

    getUserProvider works

--- src/lib/getUser.tsx
+++ src/lib/getUser.tsx
@@ -2,7 +2,6 @@
 
 import { auth, currentUser } from "@clerk/nextjs";
 import { db } from "@/lib/db";
-import { CheckUserData } from "./queries";
 
 // NOTES:
 // the CheckUser Function checks to see if the user exists in the db and adds it if not.
@@ -18,6 +17,48 @@ export async function CheckUser() {
 
   const username = `${user?.firstName} ${user?.lastName}`;
 
-  console.log(username);
-  return { user_string: userId, username: username };
+  let sqlGetUser_id = `SELECT * FROM users`; // SQL query string
+
+  let sqlInputNewUser = `INSERT INTO users (user_string, user_name) VALUES ($1, $2)`;
+
+  let sqlReturnAssociatedUserData = `SELECT
+  p.*,
+  CASE
+    WHEN upj.product_id IS NOT NULL THEN true
+    ELSE false
+  END AS user_association
+FROM
+  products p
+  LEFT JOIN user_products_junction upj ON p.id = upj.product_id
+  AND upj.user_id = ($1)`;
+
+  //get users and check if user exists
+  const getUsers = await db.query(sqlGetUser_id); // SQL query
+  const users = getUsers.rows;
+
+  // this will return falsy and trigger the query when the user is signed out.
+  // we use a truthy check for user id to ensure it is not NULL
+  const userExists = users.some((user: any) => user.user_id === userId);
+  console.log(`does the user exists? ${userExists}`);
+
+  //if user does NOT exist create new user in Database
+  if (!userExists && userId) {
+    await db.query(sqlInputNewUser, [userId, username]);
+  }
+
+  //get Data for specific user_id
+  const getAssociatedData = await db.query(sqlReturnAssociatedUserData, [
+    userId,
+  ]);
+  const userSpecificData = getAssociatedData.rows;
+
+  console.log(
+    `associated data is: ${JSON.stringify(userSpecificData, null, 2)}`
+  );
+
+  return {
+    user_id: userId,
+    username: username,
+    userSpecificData: userSpecificData,
+  };
 }
