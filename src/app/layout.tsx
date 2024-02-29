ad624d54a7e6dcd9ae5c84a6cc2df42907d64d0f
PETER ABBOTT <abbotsayshi@gmail.com>
2024-02-29 15:49:47 +0000

    lots of faff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { UserDetails } from "@/lib/getUser";
 import "./global.css";
 import TopBar from "@/components/topbar/topbar";
-
-import localFonts from "next/font/local";
-
-const civilPremium = localFonts({
-  src: "./font/CivilPremium-Regular.woff2",
-  variable: "--font-civil",
-});
-
-const inter = Inter({ subsets: ["latin"] });
+import civilFont from "../lib/custom-fonts";
 
 export const metadata: Metadata = {
   title: "Splatter",
@@ -29,7 +20,7 @@ export default async function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
+        <body className={`${civilFont.variable}`}>
           <TopBar user_id={user_id} />
           {children}
         </body>
